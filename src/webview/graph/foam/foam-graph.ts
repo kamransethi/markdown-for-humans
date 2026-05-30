95f246f4940b35d03831172e1ac664207e66c068
Kamran Sethi <kamransethi@gmail.com>
2026-05-30 10:31:31 -0400

    Update

--- src/webview/graph/foam/foam-graph.ts
+++ src/webview/graph/foam/foam-graph.ts
@@ -170,7 +170,6 @@ export class FoamGraph extends LitElement {
         .labels=${this.labels}
         .nodeFontSizeMultiplier=${this.nodeFontSizeMultiplier}
         .nodeSizeMultiplier=${this.nodeSizeMultiplier}
-        .hoverNodeId=${this.hoverNodeId}
         .linkWidthMultiplier=${this.linkWidthMultiplier}
         .animateLinks=${this.animateLinks}
         .maxFitZoom=${this.maxFitZoom}
